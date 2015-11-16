b95e9ff51154eff52cfb0bcd29e384ea68660d12
Steve Tuckner <stevetuckner@gmail.com>
2015-11-16 17:33:55 -0600

    Initial import

---
+++ Test-MVC-Net/Global.asax
@@ -0,0 +1,2 @@
+﻿<%@ Application Inherits="TestMVCNet.MvcApplication" %>
+
---
+++ Test-MVC-Net/Global.asax.cs
@@ -0,0 +1,47 @@
+﻿
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace TestMVCNet
+{
+	public class MvcApplication : System.Web.HttpApplication
+	{
+		public Thread listenerThread;
+		public AsynchronousSocketListener socketListener;
+
+		public static void RegisterRoutes (RouteCollection routes)
+		{
+			routes.IgnoreRoute ("{resource}.axd/{*pathInfo}");
+
+			routes.MapRoute (
+				"Default",
+				"{controller}/{action}/{id}",
+				new { controller = "Home", action = "Index", id = "" }
+			);
+
+		}
+
+		public static void RegisterGlobalFilters (GlobalFilterCollection filters)
+		{
+			filters.Add (new HandleErrorAttribute ());
+		}
+
+		protected void Application_Start ()
+		{
+			AreaRegistration.RegisterAllAreas ();
+			RegisterGlobalFilters (GlobalFilters.Filters);
+			RegisterRoutes (RouteTable.Routes);
+
+
+			socketListener = new AsynchronousSocketListener ();
+			listenerThread = new Thread (new ThreadStart (socketListener.StartListening));
+
+			listenerThread.Start ();
+		}
+	}
+}
